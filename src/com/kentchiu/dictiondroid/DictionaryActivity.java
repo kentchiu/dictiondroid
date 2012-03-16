23dad13a078e515a78c4eae7ff23995c9ef8daa4
Kent <kent.cwg@gmail.com>
2012-03-16 16:43:54 +0800

    first commit

---
+++ src/com/kentchiu/dictiondroid/DictionaryActivity.java
@@ -0,0 +1,13 @@
+package com.kentchiu.dictiondroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DictionaryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dictionary);
+    }
+}
